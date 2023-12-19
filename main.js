257b5d130bdf8fc67547cbba075193d0449b5866
Vinay <nk.vinay@zohomail.in>
2023-12-20 00:10:08 +0530

    Added first set of files with "related" working
    As of now, we have the following:
    - generate a response
    - generate a set of related items based on the response
    - allow user to click those "related items" to trigger new prompts

---
+++ main.js
@@ -0,0 +1,90 @@
+import { createApp, ref } from 'vue'
+import { createVuetify } from "vuetify"
+import { llm_generate, get_related } from "ollama_client"
+
+const model = "mistral"
+const ollama_url = "http://localhost:11434/api/generate"
+
+
+function parse_related(related) {
+    let lines = related.split("\n")
+    let result = []
+    for (let idx = 0; idx < lines.length; idx++) {
+        let line = lines[idx].trim()
+        if ((line[0] != "*") || (line.length < 5)) {
+            continue
+        }
+        line = line.slice(1).trim()
+        if (line.length == 0) {
+            continue
+        }
+        result.push(line)
+    }
+    return result
+}
+
+
+let appState = {
+  template: "#app-template",
+  setup() {
+    let message = ref('Hello Vue!')
+    let user_prompt = ref("")
+    let prompt_prev = ref("")
+    let response = ref("")
+    let count = ref(0)
+    let related = ref([])
+    let last_query = ""
+
+    let trigger_next = (query, real) => {
+        console.log(`query ${query}, real ${real}`)
+        response.value = ""
+        prompt_prev.value = `Generating for: ${query}`
+        last_query = query
+        related.value = []
+        if (real != null) {
+            query = real
+        }
+        llm_generate(query, set_response)
+    }
+
+    let handle_prompt_enter = (evt) => {
+        trigger_next(user_prompt.value, null)
+    }
+
+    let set_related = (msg) => {
+        related.value = parse_related(msg)
+    }
+
+    let handle_related_click = (msg) => {
+        let real = null
+        if (last_query.length > 0) {
+            real = `In the context of "${last_query}", elaborate on "${msg}"`
+        }
+        trigger_next(msg, real)
+    }
+
+    let set_response = (msg) => {
+        response.value = msg
+        prompt_prev.value = last_query
+        user_prompt.value = ""
+        related.value = ""
+        get_related(prompt_prev.value, msg, set_related)
+    }
+
+    return {
+      count,
+      prompt_prev,
+      user_prompt,
+      message,
+      response,
+      related,
+      handle_prompt_enter,
+      handle_related_click,
+    }
+  }
+}
+
+export function main() {
+    const vuetify = createVuetify()
+    createApp(appState).use(vuetify).mount('#app')
+}
---
+++ ollama_client.js
@@ -0,0 +1,65 @@
+
+const model = "mistral"
+const ollama_url = "http://localhost:11434/api/generate"
+
+function post_stream(url, data) {
+    let post_params = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    }
+    return fetch(url, post_params)
+}
+
+export async function llm_generate(text, on_success) {
+    const data = {
+        prompt: text,
+        model: model,
+        stream: true,
+        options: {
+            seed: Math.floor(Math.random() * 10000)
+        }
+    }
+    let chunks = []
+    const response = await post_stream(ollama_url, data)
+    const reader = response.body.getReader()
+    let done = false
+    while (!done) {
+        let chunk = await reader.read()
+        done = chunk.done
+        if (!chunk.value) {
+            continue
+        }
+        let decoder = new TextDecoder("utf-8")
+        let data = decoder.decode(chunk.value)
+        let obj = JSON.parse(data)
+        if (obj.response) {
+            chunks.push(obj.response)
+        }
+    }
+    on_success(chunks.join(""))
+}
+
+export function get_related(query, response, on_success) {
+    const related_query = `
+    Based on the following query and response, give a bullet list of
+    topics to explore further.
+
+    Query: ${query}
+
+    Response: ${response}
+
+    Output format required:
+    -- output --
+    * topic1
+    * topic2
+    * so on
+    --
+
+    Only names are required for topics.
+    `
+    llm_generate(related_query, on_success)
+}
+
