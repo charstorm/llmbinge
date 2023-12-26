774fef421776f6c14f3b85ca1ec80fc36ac9f576
Vinay <nk.vinay@zohomail.in>
2023-12-26 23:53:27 +0530

    Better parsing of "related" response
    - Some responses have some space before *. So a trim is added.
    - Some responses use • for bullet list instead of *. It is also
      considered in parsing now.

--- llmbinge/src/ollama_client.js
+++ llmbinge/src/ollama_client.js
@@ -81,30 +81,37 @@ function get_all_lines(data) {
 
 export async function get_related(query, response, inc_update) {
     const related_query = `
-    Based on the following query and response, give a bullet list of
+    Based on the following query and response, give suggestions for
     topics to explore further.
+
+    Include some of the following aspects, if and only if relevant and useful:
+    history, cost, cause, effect, side-effect, people, locations, timeline,
+    process, equipments, medicines, materials, risk, safety, concerns,
+    ideas, theories, simplicity, complexity, accessibility, products,
+    issues, conflicts, structure, method, etc
 
     Query: ${query}
 
     Response: ${response}
 
-    Output format required:
-    -- output --
+    Only produce 10 most relevant topics in the following output format.
+    It should be a bullet list of topics. Each topic should be short.
+    -- output format start --
     * topic1 (just a name or a phrase)
     * topic2 (just a name or a phrase)
     * so on
-    --
-
-    Only names or phrases are required for topics above.
+    -- output format end --
     `
     // let llm_output = await llm_generate(related_query)
     // let related = parse_related(llm_output)
     // return related
     let lines_added = 0
     let rem = ""
+    let full_text = ""
 
     let send_update = (line) => {
-        if (!line.startsWith("*")) {
+        line = line.trim()
+        if (!line.startsWith("*") && !line.startsWith("•")) {
             return
         }
         line = line.substr(1).trim()
@@ -116,6 +123,7 @@ export async function get_related(query, response, inc_update) {
 
     await llm_generate(related_query, (text) => {
         rem += text
+        full_text += text
         let [splits, remaining] = get_all_lines(rem)
         for (let idx = 0; idx < splits.length; idx++) {
             let part = splits[idx]
@@ -124,9 +132,11 @@ export async function get_related(query, response, inc_update) {
         rem = remaining
     })
     send_update(rem)
+    console.log(full_text)
     // Nasty hack
     // Sometimes we don't get output in the format we want. So try again.
     if (lines_added == 0) {
+        console.log("WARNING: No lines found!")
         await get_related(query, response, inc_update)
     }
 }
